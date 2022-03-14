38e5166a998c8a115bc80c21079ee362ce5b8245
Phillip Wong <deepelegant99@gmail.com>
2022-03-13 23:26:45 -0700

    Created the layout for the buttons

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,8 @@
-import React from 'react'
-import styles from './Display.module.css'
+import React from "react";
+import styles from "./Display.module.css";
 
 const Display = () => {
-  return (
-    <div className={styles.Display}>Display</div>
-  )
-}
+  return <div className={styles.display}>Display</div>;
+};
 
-export default Display
+export default Display;
--- src/components/Drumpad.js
+++ src/components/Drumpad.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import styles from './Drumpad.module.css'
+import React from "react";
+import styles from "./Drumpad.module.css";
 
 const Drumpad = () => {
   return (
-    <div className={styles.Drumpad}>Drum Pad</div>
-  )
-}
+    <div className={styles.drumpad}>
+      <button className="drum-pad">Q</button>
+      <button className="drum-pad">W</button>
+      <button className="drum-pad">E</button>
+      <button className="drum-pad">A</button>
+      <button className="drum-pad">S</button>
+      <button className="drum-pad">D</button>
+      <button className="drum-pad">Z</button>
+      <button className="drum-pad">X</button>
+      <button className="drum-pad">C</button>
+    </div>
+  );
+};
 
-export default Drumpad
+export default Drumpad;

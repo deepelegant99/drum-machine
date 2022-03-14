583eeb25106baf638a1e106e6e4d9243b15059df
Phillip Wong <deepelegant99@gmail.com>
2022-03-13 23:58:09 -0700

    Added wav Audio files

--- src/components/Drumpad.js
+++ src/components/Drumpad.js
@@ -4,15 +4,33 @@ import styles from "./Drumpad.module.css";
 const Drumpad = () => {
   return (
     <div className={styles.drumpad}>
-      <button className="drum-pad">Q</button>
-      <button className="drum-pad">W</button>
-      <button className="drum-pad">E</button>
-      <button className="drum-pad">A</button>
-      <button className="drum-pad">S</button>
-      <button className="drum-pad">D</button>
-      <button className="drum-pad">Z</button>
-      <button className="drum-pad">X</button>
-      <button className="drum-pad">C</button>
+      <button className="drum-pad" id="Q">
+        Q
+      </button>
+      <button className="drum-pad" id="W">
+        W
+      </button>
+      <button className="drum-pad" id="E">
+        E
+      </button>
+      <button className="drum-pad" id="A">
+        A
+      </button>
+      <button className="drum-pad" id="S">
+        S
+      </button>
+      <button className="drum-pad" id="D">
+        D
+      </button>
+      <button className="drum-pad" id="Z">
+        Z
+      </button>
+      <button className="drum-pad" id="X">
+        X
+      </button>
+      <button className="drum-pad" id="C">
+        C
+      </button>
     </div>
   );
 };

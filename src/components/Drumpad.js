42561d87dc2668c041a2a90f3b92ca4469886483
Henry Crossley <crossh2001@yahoo.com>
2022-03-17 00:11:03 -0700

    added audio tag

--- src/components/Drumpad.js
+++ src/components/Drumpad.js
@@ -1,38 +1,83 @@
 import React from "react";
 import styles from "./Drumpad.module.css";
+import { useState, useEffect } from "react";
 
 const Drumpad = () => {
+
+  //const [sound, setSound] = useState("");
+  const sound = document.getElementById("audio");
+  /*console.log(sound);
+
+  function playSound(){
+    sound.play();
+  }*/
+
   return (
     <div className={styles.drumpad}>
-      <button className="drum-pad" id="Q">
+      <button className="drum-pad" id="Q" onClick={playSound}>
         Q
+      <audio id="audio">
+        <source src="../Audio/de.wav" type="audio/wav"></source>
+      </audio>
       </button>
-      <button className="drum-pad" id="W">
+      <button className="drum-pad" id="W" >
         W
+      <audio id="audio1">
+        <source src="../Audio/ei.wav" type="audio/wav"></source>
+      </audio>
       </button>
       <button className="drum-pad" id="E">
         E
+      <audio id="audio2">
+        <source src="../Audio/el.wav" type="audio/wav"></source>
+      </audio>
       </button>
       <button className="drum-pad" id="A">
-        A
+        A        
+      <audio id="audio3">
+        <source src="../Audio/fe.wav" type="audio/wav"></source>
+      </audio>
       </button>
       <button className="drum-pad" id="S">
         S
+      <audio id="audio4">
+        <source src="../Audio/he.wav" type="audio/wav"></source>
+      </audio>
       </button>
       <button className="drum-pad" id="D">
         D
+      <audio id="audio5">
+        <source src="../Audio/ns.wav" type="audio/wav"></source>
+      </audio>
       </button>
       <button className="drum-pad" id="Z">
         Z
+      <audio id="audio6">
+        <source src="../Audio/re.wav" type="audio/wav"></source>
+      </audio>
       </button>
       <button className="drum-pad" id="X">
         X
+      <audio id="audio7">
+        <source src="../Audio/te.wav" type="audio/wav"></source>
+      </audio>
       </button>
       <button className="drum-pad" id="C">
         C
+      <audio id="audio8">
+        <source src="../Audio/cm.wav" type="audio/wav"></source>
+      </audio>
       </button>
     </div>
   );
+
+    function playSound(){
+    sound.play();
+  }
+
+  console.log(sound);
+
+
 };
 
 export default Drumpad;

95b2cd4f0ca86e5fe13b03f89cf16e2c85005b32
Phillip Wong <deepelegant99@gmail.com>
2022-04-27 23:07:08 -0700

    Click a button q, produce a sound, and display the key

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import Display from "./components/Display";
 import { useState } from "react";
 function App() {
   const [displayname, setDisplayname] = useState("");
+  const play = (key) => {
+    const audio = document.getElementById(key);
+    audio.currentTime = 0;
+    audio.play();
 
+    setDisplayname(key);
+    setTimeout(() => {
+      setDisplayname("");
+    }, 5000);
+  };
   return (
     <div className={styles.App} id="drum-machine">
-      <Drumpad setDisplayname={setDisplayname} />
+      <Drumpad setDisplayname={setDisplayname} play={play} />
       <Display displayname={displayname} />
     </div>
   );
